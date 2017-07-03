7a4b7c856d43083cc05c10148f73967da6486e8e
NancyAnggraini <doloksaribu.nancy@gmail.com>
2017-07-03 17:40:24 +0200

    domains and relationships set

---
+++ src/main/java/com/propulsion/recipeseBox/RecipesEBoxApplication.java
@@ -0,0 +1,12 @@
+package com.propulsion.recipeseBox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipesEBoxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipesEBoxApplication.class, args);
+	}
+}
---
+++ src/main/java/com/propulsion/recipeseBox/domain/Recipe.java
@@ -0,0 +1,58 @@
+package com.propulsion.recipeseBox.domain;
+
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.ElementCollection;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Entity //creating a table with the class name
+@Table(name = "recipes") //to change table name
+@AllArgsConstructor
+@Data
+@EqualsAndHashCode(exclude = "id")
+public class Recipe {
+
+	//fields:
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	
+	private String name;
+	
+	@ManyToOne
+	private User user;
+	
+	@ManyToMany(mappedBy = "recipes")
+	private List<Ingredient> ingredients;
+	
+	@ElementCollection
+	private List<String> methods;
+	
+	@Column(nullable = true)
+	private String photoUrl;
+	
+	@Column(nullable = true)
+	private String weblink;
+	
+	
+	//empty constructor for JPA:
+	public Recipe() {
+	}
+	
+	//constructor excluding id:
+	public Recipe(String name, User user, List<Ingredient> ingredients, List<String> methods, String photoUrl,
+			String weblink) {
+		this(null, name, user, ingredients, methods, photoUrl, weblink);
+	}
+}
